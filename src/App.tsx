293eb49b02b02d6d7c57b2d2343d39e72671b13c
Gabriela Perez Garcia <gabyperez.dev@gmail.com>
2023-11-15 14:35:02 -0600

    Added data type and api env key

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-// App.js
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+
+interface Data {
+  time: Date;
+  USD: number;
+  EUR: number;
+}
 
 const App = () => {
-  const [btcData, setBtcData] = useState(null);
-  const [ethData, setEthData] = useState(null);
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const [btcData, setBtcData] = useState<Data[]>([]);
+  const [ethData, setEthData] = useState<Data[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -11,25 +17,36 @@ const App = () => {
         "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR",
         {
           headers: {
-            authorization:
-              "Apikey 1f7b26536d7cddeb07670231a6219095065fd96b27e259b54b7de6a6d4357d34",
+            authorization: `Apikey ${apiKey}`,
           },
         }
       );
       const btcResult = await btcResponse.json();
-      setBtcData(btcResult);
+
+      setBtcData((prevBtcData) => {
+        if (prevBtcData.length === 10) {
+          return [...prevBtcData.slice(1), { time: new Date(), ...btcResult }];
+        } else {
+          return [...prevBtcData, { time: new Date(), ...btcResult }];
+        }
+      });
 
       const ethResponse = await fetch(
         "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD,EUR",
         {
           headers: {
-            authorization:
-              "Apikey 1f7b26536d7cddeb07670231a6219095065fd96b27e259b54b7de6a6d4357d34",
+            authorization: `Apikey ${apiKey}`,
           },
         }
       );
       const ethResult = await ethResponse.json();
-      setEthData(ethResult);
+      setEthData((prevEthData) => {
+        if (prevEthData.length === 10) {
+          return [...prevEthData.slice(1), { time: new Date(), ...ethResult }];
+        } else {
+          return [...prevEthData, { time: new Date(), ...ethResult }];
+        }
+      });
     };
 
     fetchData();
